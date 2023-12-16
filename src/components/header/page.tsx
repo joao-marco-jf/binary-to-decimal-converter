ccd55d127803d1eed3d54205d56c505d19a88385
joao-marco-jf <joaomarcos00512@gmail.com>
2023-12-16 11:31:16 -0300

    Atualização 16.12.2023

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -8,8 +8,8 @@ export default function Header(){
     const [isOpen, setOpen] =  useState(false)
 
     return(
-        <header className="flex justify-between p-[1rem] md:px-[4rem] text-zinc-400 border-b-[1px] border-zinc-200">
-            <h1 className="text-lg font-bold">B2D</h1>
+        <header className="flex justify-between p-[1rem] md:px-[4rem] border-b-[1px] border-zinc-200">
+            <Link href={"/"}><h1 className="text-lg font-bold">B2D<span className="font-normal">Converter</span></h1></Link>
             {isOpen &&
                 <menu className={"absolute z-50 flex flex-col gap-[1rem] p-[1rem] border-l-[1px] border-zinc-200 h-full w-[70%] md:w-[20%] bg-white top-0 right-0 md:text-lg"}>
                     <div className="w-full flex flex-row-reverse"><button onClick={() => setOpen(state => !state)}><X /></button></div>
@@ -21,6 +21,6 @@ export default function Header(){
             <button onClick={() => setOpen(state => !state)}>
                 <Menu />
             </button>
-        </header>
+        </header>   
     )
 }
